2c4401a0cac27b57ff011ffe03a41c9249d70b1b
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-11 19:05:26 +0530

    Add a new program

---
+++ Ch_3/DetectSpaces.java
@@ -0,0 +1,15 @@
+package ARC.Gitdemo.helloworld.Ch_3;
+import java.util.Scanner;
+public class DetectSpaces {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+    String letter = "";
+    System.out.println("Enter a line: ");
+    letter = scn.nextLine();
+    System.out.println(letter.trim());
+    scn.close();
+    }
+}
+
+
+//Not yet fully successful
