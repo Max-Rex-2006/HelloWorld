665d777db0e810e811ff32d2292e3c440a889c4d
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-11 18:27:33 +0530

    Add a new program

---
+++ Ch_3/EscSeqChars.java
@@ -0,0 +1,8 @@
+package ARC.Gitdemo.helloworld.Ch_3;
+
+public class EscSeqChars {
+    public static void main(String[] args) {
+        String letter = "Dear Harry, \n \t This Java Course is Nice. \nThanks.";
+        System.out.println(letter);
+    }    
+}
