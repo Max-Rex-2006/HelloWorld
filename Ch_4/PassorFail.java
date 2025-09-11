0ebc889d21145f14c8d68f84eea621b2b0d6a0a8
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-11 20:10:15 +0530

    Add a new program

---
+++ Ch_4/PassorFail.java
@@ -0,0 +1,27 @@
+package ARC.Gitdemo.helloworld.Ch_4;
+import java.util.Scanner;
+public class PassorFail {
+    public static void main(String[] args) {
+        float sub1,sub2,sub3,avg;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Please enter the marks of your 3 subjects: ");
+        sub1 = scn.nextFloat();
+        sub2 = scn.nextFloat();
+        sub3 = scn.nextFloat();
+        avg = (sub1 + sub2 + sub3)/3;
+        if (avg>=40){
+            if (sub1<=33)
+                System.out.println("You have failed.");
+            else if (sub2<=33)
+                System.out.println("You have failed.");
+            else if (sub3<=33)
+                System.out.println("You have failed.");
+            else
+                System.out.println("You have passed.");
+        }
+        else {
+            System.out.println("You have failed");
+        }
+        scn.close();
+    }
+}
