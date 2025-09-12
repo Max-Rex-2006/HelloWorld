5db0c097035f6427a058240bff9815c9b3a0d926
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-12 08:28:25 +0530

    Add a program

---
+++ Ch_4/IncomeTaxCalc.java
@@ -0,0 +1,19 @@
+package ARC.Gitdemo.helloworld.Ch_4;
+import java.util.Scanner;
+public class IncomeTaxCalc {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float income;
+        System.out.println("Please enter your income in Lakhs: ");
+        income = scn.nextFloat();
+        if (income<2.5)
+            System.out.println("You don't have to pay any tax.");
+        else if ((income>=2.5)&&(income<5.0))
+            System.out.println("You have to pay 5% tax.");
+        else if ((income>=5.0)&&(income<10.0))
+            System.out.println("You have to pay 20% tax.");
+        else 
+            System.out.println("You have to pay 30% tax");
+        scn.close();
+    }
+}
