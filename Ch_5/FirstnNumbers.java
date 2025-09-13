b407c00d99460f16b74024f70cc846c6af38cda5
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-13 22:49:10 +0530

    Add two new programs

---
+++ Ch_5/FirstnNumbers.java
@@ -0,0 +1,15 @@
+package ARC.Gitdemo.helloworld.Ch_5;
+import java.util.Scanner;
+public class FirstnNumbers {
+    public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int num=1,n;
+    System.out.println("Please enter the limit to be uploaded:");
+    n = scn.nextInt();
+    do {
+        System.out.println(num);
+        num++;
+    } while(num<=n);
+    scn.close();
+    }
+}
---
+++ Ch_5/Natural100.java
@@ -0,0 +1,11 @@
+package ARC.Gitdemo.helloworld.Ch_5;
+
+public class Natural100 {
+    public static void main(String[] args) {
+    int a = 100;
+    while (a<=200){
+        System.out.println(a);
+        a++;
+        }
+    }
+}
