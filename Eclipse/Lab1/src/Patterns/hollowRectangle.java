6df5a826fe6f4962f70700b242be082defa86b2c
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-28 08:36:58 +0530

    Add new projects

---
+++ Eclipse/Lab1/src/Patterns/hollowRectangle.java
@@ -0,0 +1,22 @@
+package Patterns;
+import java.util.Scanner;
+public class hollowRectangle {
+
+	public static void main(String[] args) {
+		int r,c,count,row;
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Please enter the number of rows:");
+		r = scn.nextInt();
+		System.out.println("Please enter the number of columns:");
+		c = scn.nextInt();
+		//outer loop
+		for (count=1; count <= c; c++) {
+			//inner loop
+			for (row =1; row <= r; r++) {
+				//cell -> (c,r)
+				if(c)
+			}
+		}
+	}
+
+}
