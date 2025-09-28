1b44247c3674193945e983ffe29c6a24dd1217fc
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-28 12:48:58 +0530

    Add a few files

---
+++ Eclipse/Lab1/src/Patterns/reverseHalfPyramid.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class reverseHalfPyramid {
+	
+	public static void main(String args[]) {
+		int c,count,row;
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Please enter the number of columns:");
+		c = scn.nextInt();
+		//outer loop for each line
+		for(count=c;count>=1;count--) {
+			for(row=1; row<=count; row++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
