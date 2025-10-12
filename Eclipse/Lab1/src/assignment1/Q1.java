053d22cff08423749ae0aafae1e6b415d91c3b84
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-10-12 22:56:43 +0530

    Add a file

---
+++ Eclipse/Lab1/src/assignment1/Q1.java
@@ -0,0 +1,21 @@
+package assignment1;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		/*
+		Write a Java Program to display following messages:
+		Hello “ITERIAN”
+		Welcome to Siksha ‘O’ Anusandhan Family.
+		Welcome to “Introduction to Computer Programming Lab”
+ 		Java is fun for All !! 
+ 		*/
+		
+		System.out.println("Hello \"ITERIAN\"");
+		System.out.println("Welcome to Siksha ‘O’ Anusandhan Family.");
+		System.out.println("Welcome to \"Introduction to Computer Programming Lab\"");
+		System.out.println("Java is fun for All !!");
+		
+	}
+
+}
